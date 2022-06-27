f18035d5e5cc984d87e2294410fcb3157b9fa4db
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-06-27 15:38:26 -0700

    ceated GiftExchange class

---
+++ models/gift-exchange.js
@@ -0,0 +1,45 @@
+class GiftExchange {
+
+    static pairs(names) {
+        if (names.length % 2 != 0) {throw new Error;}
+        var copyNames = names;
+        var finalArr = []
+        for (let idx=0; idx < (names.length)/2; idx++) {
+            var tup = []
+            for (let count=0; count < 2; count++){
+                let randomIdx = Math.random(copyNames.length)
+                let item = copyNames[randomIdx]
+                tup.splice(count,0,item)
+                copyNames.splice(randomIdx,1)
+            }
+            finalArr.splice(idx,0,tup)
+        }
+        return finalArr
+    }
+
+    
+    static traditional(names){
+        //["me", "you", "us", "her", "him", "y'all"]
+        //["they","them"]
+        var copyNames = names;
+        let randomArr = [];
+        let finalArr = [];
+        for (let idx=0; idx < names.length; idx++) {
+            let randomIdx = Math.random(copyNames.length);
+            randomArr.push(copyNames[randomIdx]);
+            copyNames.splice(randomIdx,1);
+        }
+        for (let idx=0; idx < randomArr.length; idx++){
+            let val = randomArr[idx];
+            let nextVal;
+            if (idx == randomArr.length - 1){nextVal = randomArr[0];}
+            else{nextVal = randomArr[idx + 1]}
+            let newString = `${val} is giving a gift to ${nextVal}`
+            finalArr.push(newString)
+        }
+        return finalArr;
+    }
+
+}
+
+export default GiftExchange
